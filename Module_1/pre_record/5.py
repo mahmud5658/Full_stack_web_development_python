9ff3be1ddf5b253d65ab82c2c33d8f4279f0299c
mahmud5658 <almahmuda5@gmail.com>
2024-11-10 23:00:27 +0600

    python practice

---
+++ Module_1/pre_record/5.py
@@ -0,0 +1,10 @@
+fruits = ['Apple','Banana','Orange']
+print(fruits[1])
+
+print(id(fruits))
+
+fruits.append("Cherry")
+
+print(fruits)
+
+print(id(fruits))
---
+++ Module_1/pre_record/6.py
@@ -0,0 +1,6 @@
+name = ['Abdullah','Akash','Bappy','Shuvo']
+
+print(name)
+
+name.insert(2,'Auntu')
+print(name)
