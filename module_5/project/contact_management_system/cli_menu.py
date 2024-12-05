01dad495b968126eee9bb4b02a6f6d1224145c0a
mahmud5658 <almahmuda5@gmail.com>
2024-12-05 17:36:00 +0600

    fix error in cli menu

--- module_5/project/contact_management_system/cli_menu.py
+++ module_5/project/contact_management_system/cli_menu.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\nContact Book Menu")
+    print("\nContact Book Menu\n")
     print("1. Add Contact")
     print("2. View Contact")
     print("3. Remove Contact")
@@ -9,7 +9,7 @@ def display_menu():
 
 def get_user_choice():
     try:
-        return int(input("Enter your choice: "))
+        return int(input("\nEnter your choice: "))
     except ValueError:
-        print("Invalid input. Please enter a number between 1 and 5")
+        print("\nInvalid input. Please enter a number between 1 and 5")
         return None
