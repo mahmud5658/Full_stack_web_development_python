8fed9877d9d697ab8464e4057352a9773c78507c
mahmud5658 <almahmuda5@gmail.com>
2025-01-04 19:34:17 +0600

    basic python

---
+++ python/2.py
@@ -0,0 +1,9 @@
+student ={
+    'name':'Abdullah',
+    'age':20,
+    'ID':'221-15-5658',
+    'courses':['CSE110','CSE111','CSE220','CSE221']
+}
+
+for x in student.keys():
+    print("{}:{}".format(x,student[x]))
